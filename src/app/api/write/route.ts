3f53adba2a80446bd746f8e4567fe5e4a795f2d0
@ashappdev <ashley.rudland@outlook.com>
2024-04-09 21:18:58 +0100

    fix build

--- src/app/api/write/route.ts
+++ src/app/api/write/route.ts
@@ -1,6 +1,8 @@
+import { sql } from 'drizzle-orm';
+import { NextRequest } from 'next/server';
+
 import { comments } from '@/models/schema';
 import { db } from '@/utils/db';
-import { sql } from 'drizzle-orm';
 
 const insertQuery = db
 	.insert(comments)
@@ -10,11 +12,17 @@ const insertQuery = db
 	})
 	.prepare();
 
-export async function POST() {
-	const { lastInsertRowid } = await insertQuery.execute({
-		author: Math.random().toString(36).substring(7),
-		content: Math.random().toString(36).substring(7),
-	});
+export async function POST(request: NextRequest) {
+	const runTest = request.nextUrl.searchParams.get('test') === 'y';
 
-	return new Response(lastInsertRowid.toString(), { status: 200 });
+	if (runTest) {
+		const { lastInsertRowid } = await insertQuery.execute({
+			author: Math.random().toString(36).substring(7),
+			content: Math.random().toString(36).substring(7),
+		});
+
+		return new Response(lastInsertRowid.toString(), { status: 200 });
+	}
+
+	return new Response('No test run', { status: 200 });
 }
