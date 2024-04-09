b522cbac4416a5ca3d78a26b1f07ccd2689d8325
@ashappdev <ashley.rudland@outlook.com>
2024-04-09 07:10:53 +0100

    find out why /up takes 30 seconds?

--- src/app/up/route.ts
+++ src/app/up/route.ts
@@ -1,19 +1,6 @@
-// default uptime kamal check route, see: https://kamal-deploy.org/docs/configuration/healthchecks/
+console.log(`${new Date().toISOString()}: /up route.ts loaded`);
 
-import os from 'os';
-import { cookies } from 'next/headers';
-import { NextRequest } from 'next/server';
-
-const startTime = new Date().toISOString();
-
-export async function GET(request: NextRequest) {
-	// make dynamic
-	cookies().get('auth');
-
-	return Response.json({
-		startTime,
-		now: new Date().toISOString(),
-		url: request.nextUrl,
-		uptime: os.uptime(),
-	});
+export async function GET() {
+	console.log(`${new Date().toISOString()}: /up GET hit`);
+	return new Response('Ok', { status: 200 });
 }
